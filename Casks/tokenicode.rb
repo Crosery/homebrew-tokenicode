978ef39dc46f6406f5142073d412fdbac48a6f75
Crosery <luoxi2024@gmail.com>
2026-05-29 08:51:04 +0000

    feat: update to 0.10.8

--- Casks/tokenicode.rb
+++ Casks/tokenicode.rb
@@ -1,14 +1,14 @@
 cask "tokenicode" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.10.7"
+  version "0.10.8"
 
   on_arm do
-    sha256 "aeb48705667a92c84e9854e9731be25038ba039eb2833577f2ca6d66bb827a57"
+    sha256 "54bc1ee5e28eedfb24b285b69f90c7ad485f24fa48d03798056f33e8d0e8cc3b"
   end
 
   on_intel do
-    sha256 "0c25df1301e1447c2482e8b0c3bdccce57463a9882d20e8af59911e8668edecd"
+    sha256 "83ec64ecf57aa3ad144c643ebe82e39b07fcbc9086d16af465612878a71984d4"
   end
 
   url "https://github.com/yiliqi78/TOKENICODE/releases/download/v#{version}/TOKENICODE_#{version}_#{arch}.dmg"
